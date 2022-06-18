7f486a3ca9aaa57c7806ba25a122682a7c164c32
Remix Workspace <>
2022-06-18 13:07:46 +0530

    Learnt how to use Chain.link pricefeeds

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,31 +4,30 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe{
     int public favNumber ;
-    int public latestPriceOfBTCinETH;
-    address public feedAddress = 0xF7904a295A029a3aBDFFB6F12755974a958C7C25;
+    uint256 public minimumUSDToSend = 1 * 1e18;
+    address public feedAddress = 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e;
     AggregatorV3Interface internal priceFeed;
-      function fund () public payable{
-        favNumber = 5;
-        require(msg.value >= 1, "not enough fund");
-        latestPriceOfBTCinETH = getLatestPrice();
+    function fund () public payable{
+        require(convertEthToDollars(msg.value) >= minimumUSDToSend, "not enough fund");
     }
-    
     
     constructor() {
         priceFeed = AggregatorV3Interface(feedAddress);
     }
+
+    function convertEthToDollars(uint256 ethValue) public view returns (uint256){
+        uint256 priceInUSD = uint256(getLatestPriceOfEthInUSD());
+        return (priceInUSD * (10 ** (18 - getDecimalsInReturnedValueByThePriceFeed())) * ethValue) /1e18;
+    }
   
-    function getLatestPrice() public view returns (int) {
-        (
-            /*uint80 roundID*/,
-            int price,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
-        ) = priceFeed.latestRoundData();
+    function getLatestPriceOfEthInUSD() public view returns (int256) {
+        (,int256 price,,,) = priceFeed.latestRoundData();
         return price;
+    }
+    function getDecimalsInReturnedValueByThePriceFeed() public view returns (uint8){
+        return priceFeed.decimals();
     }
 }
 
-//1btc = 18.964140602914910000 eth
+//1095_25000000
 
