548abdd41cbf519c861efda2d3c72acd21ff0dcb
Remix Workspace <>
2022-06-22 13:50:30 +0530

    Added a withdraw function through which the owner can withdraw the funds

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,9 +4,37 @@ import "./PriceConverter.sol";
 contract FundMe{
     using PriceConverter for uint256 ;
     int public favNumber ;
-    uint256 public minimumUSDToSend = 1 * 1e18;
+    uint256 public MINIMUM_USD = 1 * 1e18;
+    address[] public funders;
+    address public i_owner;
+    mapping(address=>uint256) public funderAddressToAmount;
+    
+    constructor(){
+        i_owner = msg.sender;
+    }
+
     function fund () public payable{
-        require(msg.value.convertEthToDollars() >= minimumUSDToSend, "not enough fund");
+        require(msg.value.convertEthToDollars() >= MINIMUM_USD, "not enough fund");
+        funders.push(msg.sender);
+        funderAddressToAmount[msg.sender] += msg.value;
+    }
+
+    function withdraw() public onlyOwner{
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            funderAddressToAmount[funder] = 0;
+        }
+        funders = new address[](0);
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess,"Call failed");
     }
     
+    modifier onlyOwner(){
+        require(msg.sender == i_owner, "Sender is not the owner");
+        _;
+    }
 }
+
+//Resources
+
+//https://jeancvllr.medium.com/solidity-tutorial-all-about-libraries-762e5a3692f9
